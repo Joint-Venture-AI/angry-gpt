e5fe8f37a6a0cbdf49276241e515882f08bc6aec
shaishabcoding <shaishabchandrashil@gmail.com>
2025-03-10 22:05:37 +0600

    refactor: Simplify auth middleware using catchAsync utility

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -1,48 +1,37 @@
-import { NextFunction, Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import ServerError from '../../errors/ServerError';
 import User from '../modules/user/User.model';
 import { verifyToken } from '../modules/auth/Auth.utils';
+import catchAsync from '../../shared/catchAsync';
 
 /**
  * Middleware to authenticate and authorize requests based on user roles
  *
  * @param roles - The roles that are allowed to access the resource
  */
-const auth =
-  (...roles: ('USER' | 'ADMIN')[]) =>
-  async (req: Request, _res: Response, next: NextFunction) => {
-    try {
-      const token = req.headers?.authorization?.split(' ')[1];
+const auth = (...roles: ('USER' | 'ADMIN')[]) =>
+  catchAsync(async (req, _, next) => {
+    const token = req.headers?.authorization?.split(' ')[1];
 
-      if (!token)
-        throw new ServerError(
-          StatusCodes.UNAUTHORIZED,
-          'You are not authorized',
-        );
+    if (!token)
+      throw new ServerError(StatusCodes.UNAUTHORIZED, 'You are not authorized');
 
-      const { email } = verifyToken(token, 'access');
+    const { email } = verifyToken(token, 'access');
 
-      const user = await User.findOne({ email });
+    const user = await User.findOne({ email });
 
-      if (!user)
-        throw new ServerError(
-          StatusCodes.UNAUTHORIZED,
-          'You are not authorized',
-        );
+    if (!user)
+      throw new ServerError(StatusCodes.UNAUTHORIZED, 'You are not authorized');
 
-      req.user = user;
+    req.user = user;
 
-      if (roles.length && !roles.includes(user.role))
-        throw new ServerError(
-          StatusCodes.FORBIDDEN,
-          'You are not authorized to access this api',
-        );
+    if (roles.length && !roles.includes(user.role))
+      throw new ServerError(
+        StatusCodes.FORBIDDEN,
+        'You are not authorized to access this api',
+      );
 
-      next();
-    } catch (error) {
-      next(error);
-    }
-  };
+    next();
+  });
 
 export default auth;
