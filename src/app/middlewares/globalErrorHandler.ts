bfa976a022de4ad22e295e3486597058b48e8a08
shaishabcoding <shaishabchandrashil@gmail.com>
2025-03-24 18:03:29 +0600

    fixed

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,14 +1,16 @@
-/* eslint-disable no-console, no-unused-vars, @typescript-eslint/no-unused-vars, no-unused-expressions */
+/* eslint-disable no-unused-vars, @typescript-eslint/no-unused-vars, no-console */
 import { ErrorRequestHandler } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import colors from 'colors';
+import { ZodError } from 'zod';
+import mongoose from 'mongoose';
 import config from '../../config';
-import handleValidationError from '../../errors/handleValidationError';
-import handleZodError from '../../errors/handleZodError';
 import ServerError from '../../errors/ServerError';
+import handleZodError from '../../errors/handleZodError';
+import handleValidationError from '../../errors/handleValidationError';
+import handleMongooseDuplicateError from '../../errors/handleMongooseDuplicateError';
 import { errorLogger } from '../../util/logger/logger';
 import { TErrorHandler, TErrorMessage } from '../../types/errors.types';
-import handleMongooseDuplicateError from '../../errors/handleMongooseDuplicateError';
 
 const defaultError: TErrorHandler = {
   statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
@@ -16,61 +18,36 @@ const defaultError: TErrorHandler = {
   errorMessages: [],
 };
 
-/**
- * Global error handler middleware
- *
- * This middleware catches all errors and logs them appropriately based on the environment
- */
 const globalErrorHandler: ErrorRequestHandler = (error, _req, res, _next) => {
-  config.server.node_env === 'development'
-    ? console.log(colors.red('🚨 globalErrorHandler ~~ '), error)
-    : errorLogger.error(colors.red('🚨 globalErrorHandler ~~ '), error);
+  if (config.server.node_env === 'development') {
+    console.log(colors.red('🚨 globalErrorHandler ~~ '), error);
+  } else {
+    errorLogger.error(colors.red('🚨 globalErrorHandler ~~ '), error);
+  }
 
-  const { statusCode, message, errorMessages } = handleError(error);
+  const { statusCode, message, errorMessages } = formatError(error);
 
   res.status(statusCode).json({
     success: false,
     message,
     errorMessages,
-    stack: config.server.node_env !== 'production' ? error?.stack : undefined,
+    stack: config.server.node_env !== 'production' ? error.stack : undefined,
   });
 };
 
 export default globalErrorHandler;
 
-const createErrorMessage = (message: string): TErrorMessage[] => [
-  { path: '', message },
-];
-
-const handleError = (error: any): TErrorHandler => {
-  const errorHandlers: Record<string, () => TErrorHandler> = {
-    ZodError: () => handleZodError(error),
-    ValidationError: () => handleValidationError(error),
-    TokenExpiredError: () => ({
-      statusCode: StatusCodes.UNAUTHORIZED,
-      message: 'Session Expired',
-      errorMessages: createErrorMessage(
-        'Your session has expired. Please log in again to continue.',
-      ),
-    }),
-    JsonWebTokenError: () => ({
-      statusCode: StatusCodes.UNAUTHORIZED,
-      message: 'Invalid Token',
-      errorMessages: createErrorMessage(
-        'Your token is invalid. Please log in again to continue.',
-      ),
-    }),
-  };
-
+const formatError = (error: any): TErrorHandler => {
+  if (error instanceof ZodError) return handleZodError(error);
+  if (error instanceof mongoose.Error.ValidationError)
+    return handleValidationError(error);
   if (error.code === 11000) return handleMongooseDuplicateError(error);
-
   if (error instanceof ServerError)
     return {
       statusCode: error.statusCode,
       message: error.message,
       errorMessages: createErrorMessage(error.message),
     };
-
   if (error instanceof Error)
     return {
       ...defaultError,
@@ -78,5 +55,9 @@ const handleError = (error: any): TErrorHandler => {
       errorMessages: createErrorMessage(error.message),
     };
 
-  return errorHandlers[error.name]?.() ?? defaultError;
+  return defaultError;
 };
+
+const createErrorMessage = (message: string): TErrorMessage[] => [
+  { path: '', message },
+];
