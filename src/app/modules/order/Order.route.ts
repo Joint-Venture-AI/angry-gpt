fcddde663862028ded43479f82297c47dffa5add
shaishabcoding <shaishabchandrashil@gmail.com>
2025-03-26 17:35:19 +0600

    cleanup

--- src/app/modules/order/Order.route.ts
+++ src/app/modules/order/Order.route.ts
@@ -5,47 +5,32 @@ import { QueryValidations } from '../query/Query.validation';
 import Order from './Order.model';
 import { OrderValidation } from './Order.validation';
 
-const publicRouter = Router();
-const privateRouter = Router();
+const router = Router();
 
-publicRouter.get(
-  '/',
-  purifyRequest(QueryValidations.list),
-  OrderController.list,
-);
+router.get('/', purifyRequest(QueryValidations.list), OrderController.list);
 
-publicRouter.get(
+router.get(
   '/:orderId',
   purifyRequest(QueryValidations.exists('orderId', Order)),
   OrderController.retrieve,
 );
 
-publicRouter.post('/checkout', OrderController.checkout);
+router.post('/checkout', OrderController.checkout);
 
-publicRouter.post(
+router.post(
   '/:orderId/cancel',
   purifyRequest(QueryValidations.exists('orderId', Order)),
   OrderController.cancel,
 );
 
-/**
- * *************************************************************************************************************
- *                                                                                                           *
- *                                           L I N E   B R E A K                                           *
- *                                                                                                           *
- * **************************************************************************************************************
- */
-
-privateRouter.patch(
-  '/:orderId/:state',
-  purifyRequest(
-    QueryValidations.exists('orderId', Order),
-    OrderValidation.state,
-  ),
-  OrderController.changeState,
-);
-
 export const OrderRoutes = {
-  user: publicRouter,
-  admin: privateRouter,
+  user: router,
+  admin: Router().patch(
+    '/:orderId/:state',
+    purifyRequest(
+      QueryValidations.exists('orderId', Order),
+      OrderValidation.state,
+    ),
+    OrderController.changeState,
+  ),
 };
