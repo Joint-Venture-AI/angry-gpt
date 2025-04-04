7b23f0bab14750b1e3ed560f6fbcf606408b8fd4
shaishabcoding <shaishabchandrashil@gmail.com>
2025-04-04 20:00:40 +0600

    cleanup

--- src/app/modules/setting/Setting.model.ts
+++ src/app/modules/setting/Setting.model.ts
@@ -1,16 +1,21 @@
 import { Schema, model } from 'mongoose';
 import { TSetting } from './Setting.interface';
 
-const settingSchema = new Schema<TSetting>({
-  name: {
-    type: String,
-    required: true,
+const settingSchema = new Schema<TSetting>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    value: {
+      type: String,
+      required: true,
+    },
   },
-  value: {
-    type: String,
-    required: true,
+  {
+    versionKey: false,
   },
-});
+);
 
 const Setting = model<TSetting>('Setting', settingSchema);
 
